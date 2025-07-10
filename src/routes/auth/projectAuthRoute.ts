bc99d3d791ec9f23af69be6ca7bd4eec2152a564
Valexjnr54 <jimvalex54@gmail.com>
2025-07-10 03:42:50 -0700

    Fixing the project manager router to correctly map to the actual change temp password controller

--- src/routes/auth/projectAuthRoute.ts
+++ src/routes/auth/projectAuthRoute.ts
@@ -2,7 +2,7 @@
 import express from 'express';
 import { upload } from '../../middlewares/multerMiddleware';
 import { authenticateJWT } from '../../middlewares/authMiddleware/authenticationMiddleware';
-import { changeProjectManagerPassword, loginProjectManager, logoutProjectManager, verifyProjectEmail } from '../../controllers/Authentication/projectManagerAuthController';
+import { changeProjectManagerPassword, changeProjectManagerTemporalPassword, loginProjectManager, logoutProjectManager, verifyProjectEmail } from '../../controllers/Authentication/projectManagerAuthController';
 
 export const projectAuthRouter = express.Router();
 
@@ -11,4 +11,4 @@ projectAuthRouter.post('/login', loginProjectManager);
 projectAuthRouter.post('/logout', authenticateJWT, logoutProjectManager);
 projectAuthRouter.post('/email-verification', authenticateJWT, verifyProjectEmail);
 projectAuthRouter.post('/change-password', authenticateJWT, changeProjectManagerPassword);
-projectAuthRouter.post('/change-temp-password', authenticateJWT, changeProjectManagerPassword);
+projectAuthRouter.post('/change-temp-password', authenticateJWT, changeProjectManagerTemporalPassword);
